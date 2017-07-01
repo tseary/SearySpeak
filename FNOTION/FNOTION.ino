acf75948e892515f48783e73c013c9b71ae9fa12
Thomas Seary <tseary@gmail.com>
2017-07-01 21:28:03 -0230

    First test of reading txt file

--- FNOTION/FNOTION.ino
+++ FNOTION/FNOTION.ino
@@ -24,8 +24,10 @@ MorseTx morse(BEACON_EN_PIN);
 #endif
 
 // SD card
-SdFat sd;		// File system object
-SdFile file;	// Log file
+SdFat sdFat;	// File system object
+const char* MORSE_FILE_NAME = "Transmit.txt";
+File morseFile;	// Morse code file	TODO Figure out how to use SdFile for both
+SdFile logFile;	// Log file
 
 /******************************************************************************
  * Setup / Initializers
@@ -35,15 +37,26 @@ void setup() {
 
 #ifdef _DEBUG
 	Serial.begin(9600);
-	while (!Serial);
+	while (!Serial.available());
 	Serial.println("SearySpeak v1.0 - " __TIMESTAMP__);
 #endif
-	
+
 	initializeBeacon();
 
 	Diagnostics::initialize();
 	Sensors::initialize();
 	SolarCharger::initialize();
+
+	initializeSD();
+
+	// Open morse code file
+	// TODO If morse file doesn't exist, create it with readme comments
+	morseFile = sdFat.open(MORSE_FILE_NAME, O_READ | O_EXCL);
+	if (!morseFile.open(MORSE_FILE_NAME, O_READ/* | O_EXCL*/)) {
+		//error("file.open");
+		Serial.print("Error opening morse file: ");
+		Serial.println(MORSE_FILE_NAME);
+	}
 
 	dataLoggerSetup();
 }
@@ -58,12 +71,20 @@ void initializeBeacon() {
 	morse.setShortFist(0.25); // Shorter pulses to save power
 }
 
+void initializeSD() {
+	// Initialize at the highest speed supported by the board that is
+	// not over 50 MHz. Try a lower speed if SPI errors occur.
+	if (!sdFat.begin(SD_SS_PIN, SD_SCK_MHZ(50))) {
+		sdFat.initErrorHalt();
+	}
+}
+
 /******************************************************************************
  * Main Loop
  ******************************************************************************/
 
 void loop() {
-/*#ifdef DEBUG
+/*#ifdef _DEBUG
 	morse.write(SEARYSPEAK_URL);
 #endif*/
 
@@ -73,14 +94,23 @@ void loop() {
 	dataLoggerLoop();
 
 	Diagnostics::updateRuntime();
-	Serial.print("Runtime:\t");
+
+	/*Serial.print("Runtime:\t");
 	Serial.println(Diagnostics::getRuntime());
 	Serial.print("millis:\t");
 	Serial.println(millis());
-	Serial.println();
+	Serial.println();*/
 
 	// Nightfall
-	// TODO Load block of names from SD card into RAM
+	// TODO Load one line from SD card into RAM
+	const uint16_t LINE_LENGTH = 100;
+	static char transmitLine[LINE_LENGTH];
+	int readReturn = csvReadText(&morseFile, transmitLine, LINE_LENGTH, '\n');
+	Serial.print(readReturn);
+	Serial.print('\t');
+	if (readReturn >= 0) {
+		Serial.println(transmitLine);
+	}
 
 	// TODO Transmit names interspersed with URL
 
@@ -99,7 +129,7 @@ void loop() {
 
  // TODO clean up all of this
 void dataLoggerSetup() {
-
+	// Create file name
 	const uint8_t BASE_NAME_SIZE = sizeof(FILE_BASE_NAME) - 1;
 	char fileName[BASE_NAME_SIZE + 7] = FILE_BASE_NAME "00.csv";	// Make 8.3 name
 
@@ -107,14 +137,8 @@ void dataLoggerSetup() {
 		//error("FILE_BASE_NAME too long");
 	}
 
-	// Initialize at the highest speed supported by the board that is
-	// not over 50 MHz. Try a lower speed if SPI errors occur.
-	if (!sd.begin(SD_SS_PIN, SD_SCK_MHZ(50))) {
-		sd.initErrorHalt();
-	}
-
 	// Find an unused file name.
-	while (sd.exists(fileName)) {
+	while (sdFat.exists(fileName)) {
 		if (fileName[BASE_NAME_SIZE + 1] != '9') {
 			fileName[BASE_NAME_SIZE + 1]++;	// Increment 1's place
 		} else if (fileName[BASE_NAME_SIZE] != '9') {
@@ -125,12 +149,12 @@ void dataLoggerSetup() {
 		}
 	}
 
-	if (!file.open(fileName, O_CREAT | O_WRITE | O_EXCL)) {
+	if (!logFile.open(fileName, O_CREAT | O_WRITE | O_EXCL)) {
 		//error("file.open");
 	}
 
 	// Write data header
-	file.println(F("runtime,light,temperature,voltage,charging,done"));
+	logFile.println(F("runtime,light,temperature,voltage,charging,done"));
 }
 
 void dataLoggerLoop() {
@@ -161,29 +185,29 @@ void dataLoggerLoop() {
 #endif*/
 
 	// Write time stamp
-	file.print(Diagnostics::getRuntime());
+	logFile.print(Diagnostics::getRuntime());
 
 	// Write sensor readings
-	file.write(CSV_DELIMITER);
-	file.print(ambientLight);
-	file.write(CSV_DELIMITER);
-	file.print(temperature);
-	file.write(CSV_DELIMITER);
-	file.print(loadVoltage, VOLTAGE_DIGITS);
+	logFile.write(CSV_DELIMITER);
+	logFile.print(ambientLight);
+	logFile.write(CSV_DELIMITER);
+	logFile.print(temperature);
+	logFile.write(CSV_DELIMITER);
+	logFile.print(loadVoltage, VOLTAGE_DIGITS);
 
 	// Write charging information
-	file.write(CSV_DELIMITER);
-	file.print(charging);
-	file.write(CSV_DELIMITER);
-	file.print(chargingDone);
+	logFile.write(CSV_DELIMITER);
+	logFile.print(charging);
+	logFile.write(CSV_DELIMITER);
+	logFile.print(chargingDone);
 
 	// Write newline
-	file.println();
+	logFile.println();
 
 	// Force data to SD and update the directory entry to avoid data loss
-	// file.sync() is like close and open, but more efficient
+	// logFile.sync() is like close and open, but more efficient
 	// TODO How to make robust error recovery?
-	if (!file.sync() || file.getWriteError()) {
+	if (!logFile.sync() || logFile.getWriteError()) {
 		//error("write error");
 		setStatusLight(true);
 		delay(1000);
@@ -213,4 +237,54 @@ void dataLoggerLoop() {
 
 void setStatusLight(bool enable) {
 	digitalWrite(STATUS_LED_PIN, enable);
+}
+
+// Read one entry from a csv file.
+// str is the output string buffer.
+// maxSize is the size of str including terminating null.
+// delim is typically ',' (default) but can be any char.
+// Returns:
+// 0 if end of file
+// -1 if read error
+// -2 if overflow (entry too long)
+// delimiter if successful (delim or '\n')
+int csvReadText(File* file, char* str, uint16_t maxSize, char delim /*= ','*/) {
+	char c;
+	int rtn;
+	uint16_t n = 0;
+	while (true) {
+		// Check for end of file
+		if (!file->available()) {
+			rtn = 0;
+			break;
+		}
+
+		// Check string size (leave space for null)
+		if (n >= maxSize) {
+			rtn = -2;
+			break;
+		}
+
+		// Read next char
+		if (file->read(&c, 1) != 1) {
+			rtn = -1;	// Read error
+			break;
+		}
+
+		// Delete CR
+		if (c == '\r') {
+			continue;
+		}
+
+		// Check for delimiter
+		if (c == delim || c == '\n') {
+			rtn = c;
+			break;
+		}
+
+		// Append char to string
+		str[n++] = c;
+	}
+	str[n] = '\0';	// Terminate string
+	return rtn;
 }
